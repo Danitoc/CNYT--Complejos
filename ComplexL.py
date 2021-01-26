cee3174fe9627897843b986422ea9bb097eebfcc
Daniel Villami Gómez <daniel.villamil@mail.escuelaing.edu.co>
2021-01-25 19:42:40 -0500

    Funciones documentadas

--- ComplexL.py
+++ ComplexL.py
@@ -1,27 +1,38 @@
 import math as m
 
-'''Para el uso de las sigueintes funciones se tiene en cuenta
-que los parametros c1 y c2 hacen referencia a tuplas de numeros 
-complejos.
-'''
 
-
-#                               Operaciones con numeros complejos
+#                               Operaciones con números complejos
 
 
 def SumaComplejos(c1, c2):
+    '''
+    Los parametros c1 y c2 son números complejos
+    Función que suma números complejos
+    '''
     return c1[0] + c2[0], c1[1] + c2[1]
 
 
 def MultiplicacionComplejos(c1, c2):
+    '''
+    Los parametros c1 y c2 son números complejos
+    Función que multiplica números complejos
+    '''
     return c1[0] * c2[0] - c1[1] * c2[1], c1[0] * c2[1] + c2[0] * c1[1]
 
 
 def RestaComplejos(c1, c2):
+    '''
+    Los parametros c1 y c2 son números complejos
+    Función que resta números complejos
+    '''
     return c1[0] - c2[0], c1[1] - c2[1]
 
 
 def DivisionComplejos(c1, c2):
+    '''
+    Los parametros c1 y c2 son números complejos
+    Función que divide números complejos
+    '''
     r1 = c1[0] * c2[0] + c1[1] * c2[1]
     r2 = c2[0] * c1[1] - c1[0] * c2[1]
     div = c2[0] ** 2 + c2[1] ** 2
@@ -29,10 +40,18 @@ def DivisionComplejos(c1, c2):
 
 
 def ModuloComplejos(c):
+    '''
+    El parámetro c es un número complejo
+    Función que calcula el modulo de un número complejo
+    '''
     return (c[0] ** 2 + c[1] ** 2) ** 1 / 2
 
 
 def ConjugadoComplejos(c):
+    '''
+    El parámetro c es un número complejo
+    Función que calcula el conjugado de un número complejo
+    '''
     return c[0], -c[1]
 
 
@@ -40,10 +59,18 @@ def ConjugadoComplejos(c):
 
 
 def GradosRad(dat):
+    '''
+    El parámetro dat es un ángulo
+    Función que transforma de grados a radianes
+    '''
     return (dat * m.pi) / 180
 
 
 def RadGrados(dat):
+    '''
+    El parámetro dat es un radian
+    Función que transforma de radianes a grados
+    '''
     return (dat * 180) / m.pi
 
 
@@ -51,14 +78,26 @@ def RadGrados(dat):
 
 
 def CartesianaPolar(c):
+    '''
+    El parámetro c es un número complejo
+    Función que transforma de coordenadas cartesianas a polares
+    '''
     return m.sqrt(c[0] ** 2 + c[1] ** 2), RadGrados(m.atan2(c[1], abs(c[0])))
 
 
 def PolaCartesiano(p):
+    '''
+    El parámetro p es una coordenada polar
+    Función que transforma de coordenadas polares a cartesianas
+    '''
     return p[0] * m.cos(GradosRad(p[1])), p[0] * m.sin(GradosRad(p[1]))
 
 
 def fase(c):
+    '''
+    El parámetro c es un número complejo
+    Función que calcula la fase de un número complejo
+    '''
     return m.atan2(c[1], c[0])
 
 
@@ -66,6 +105,10 @@ def fase(c):
 
 
 def InvesoComplejo(c):
+    '''
+    El parámetro c es un número complejo
+    Función que calcula el inverso aditivo de un número complejo
+    '''
     return c[0] * -1, c[1] * -1
 
 
@@ -73,6 +116,10 @@ def InvesoComplejo(c):
 
 
 def SumaVectores(v1, v2):
+    '''
+    Los parámetros v1 y v2 son vectores de mismo tamaño compuestos por números complejos
+    Función que suma vectores
+    '''
     vec = list()
     for i in range(0, len(v1)):
         vec.append(SumaComplejos(v1[i], v2[i]))
@@ -80,6 +127,10 @@ def SumaVectores(v1, v2):
 
 
 def InversoAditivoVec(v):
+    '''
+    El parámetro v es un vector compuesto de números complejos
+    Función que calcula el inverso aditivo de un vector complejo
+    '''
     vec = list()
     for i in range(0, len(v)):
         vec.append(InvesoComplejo(v[i]))
@@ -87,6 +138,10 @@ def InversoAditivoVec(v):
 
 
 def VecEscalar(e, v):
+    '''
+    Los parámetros e y v son un número complejo y un vector de complejos respectivamente
+    Función que multiplica un número complejo por un vector de complejos
+    '''
     vec = list()
     for i in range(0, len(v)):
         vec.append(MultiplicacionComplejos(e, v[i]))
